66c3560eb4cad472826c48309492ddacdf0c4ed2
Živanić Vanja <s51645@edu.campus02.at>
2022-03-02 14:05:42 +0100

    new

---
+++ src/com/company/Rectangle.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Rectangle {
+    public static void main(String[] args) {
+        int width = 5;
+        int lenght = 4;
+        int circumferenceOfRectangle = 2 * (lenght) + 2 * (width);
+        int areaOfRectangle = lenght * width;
+        System.out.println("Circumference of rectangle is: " + circumferenceOfRectangle);
+        System.out.println("Area of rectangle is: " + areaOfRectangle);
+    }
+}
