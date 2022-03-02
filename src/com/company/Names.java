822a3deeccf1d46aec98c49cafa82e728e394ba7
Živanić Vanja <s51645@edu.campus02.at>
2022-03-02 13:45:03 +0100

    Initial commit

---
+++ src/com/company/Names.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Names {
+
+    public static void main(String[] args) {
+	String name = "Jane";
+	String surname = "Doe";
+        System.out.println("The full name is " + name + " " + surname + ".");
+
+    }
+}
